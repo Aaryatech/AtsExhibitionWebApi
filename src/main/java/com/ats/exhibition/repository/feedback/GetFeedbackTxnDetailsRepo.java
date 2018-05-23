3aea98a14deaaf467446b2879a1be5b003454abe
Aaryatech <code@atsinfosoft.com>
2018-05-23 12:28:12 +0530

    fbTxn Detail//Sac

--- src/main/java/com/ats/exhibition/repository/feedback/GetFeedbackTxnDetailsRepo.java
+++ src/main/java/com/ats/exhibition/repository/feedback/GetFeedbackTxnDetailsRepo.java
@@ -8,11 +8,11 @@ import com.ats.exhibition.model.feedback.GetFeedbackTxnDetails;
 
 public interface GetFeedbackTxnDetailsRepo extends JpaRepository<GetFeedbackTxnDetails, Integer> {
 	
-	
 	@Query(value = " SELECT m_feedback_que.question,m_feedback_que.que_desc,t_feedback.fb_tx_id,t_feedback.exhb_id,t_feedback.fb_id,t_feedback.event_id," + 
 			" t_feedback.rating,t_feedback.remark,m_visitor.visitor_id,m_visitor.visitor_name,m_visitor.visitor_mobile " + 
 			" FROM t_feedback,m_feedback_que,m_visitor " + 
-			" WHERE t_feedback.user_id=m_visitor.visitor_id AND t_feedback.fb_id=m_feedback_que.fb_id AND t_feedback.fb_id=:fbId ", nativeQuery = true)
+			" WHERE t_feedback.user_id=m_visitor.visitor_id AND t_feedback.fb_id=m_feedback_que.fb_id AND t_feedback.fb_id=:fbId  "
+			+ "ORDER BY t_feedback.fb_tx_id DESC ", nativeQuery = true)
 	List<GetFeedbackTxnDetails> getFbQueDetailByFbId(@Param("fbId") int fbId);
 	
 
